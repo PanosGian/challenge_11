b78fa2fdd273a0f0d4d0a20a6c41ba65a20faa1f
Panos <gia-pan@hotmail.com>
2023-03-18 16:15:05 +0800

    first draft

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,43 @@
-// Dependencies
+
 const path = require('path');
 const fs = require('fs');
 const uniqid = require('uniqid');
+const express = require('express');
+const router = express.Router();
 
-// Routing
-module.exports = (app) => {
+// Read in the db.json file and return as a response all saved notes as JSON.
+router.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../db/db.json'));
+});
 
-  // Read the db.json file and return all saved notes as JSON.
-  app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-  });
+// get the new note and save it in the db.jon file ie. the request body, add return the new note to the client.
+router.post('/api/notes', (req, res) => {
+  let db = JSON.parse(fs.readFileSync('db/db.json'));
 
-  // Receive a new note to save on the request body, add it to the db.json file, and return the new note to the client.
-  app.post('/api/notes', (req, res) => {
-    let db = JSON.parse(fs.readFileSync('db/db.json'));
+  // Creating the body for the note
+  const userNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uniqid(),
+  };
 
-    // Creating the body for the note
-    const userNote = {
-      title: req.body.title,
-      text: req.body.text,
-      id: uniqid(),
-    };
+  // Append the new note to the db.json file
+  db.push(userNote);
+  fs.writeFileSync('db/db.json', JSON.stringify(db));
+  res.json(userNote);
+});
 
-    // Adding the created note to the db.json file
-    db.push(userNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.json(userNote);
-  });
+// Receive the id of a note to delete.
+router.delete('/api/notes/:id', (req, res) => {
+  const db = JSON.parse(fs.readFileSync('db/db.json'));
 
-  // Receive a query parameter containing the id of a note to delete.
-  app.delete('/api/notes/:id', (req, res) => {
-    const db = JSON.parse(fs.readFileSync('db/db.json'));
+  // Filtering out the note with the provided id
+  const updatedNotes = db.filter(item => item.id !== req.params.id);
 
-    // Filtering out the note with the given id
-    const updatedNotes = db.filter(item => item.id !== req.params.id);
+  // update the db.json file
+  fs.writeFileSync('db/db.json', JSON.stringify(updatedNotes));
+  res.json(updatedNotes);
+});
 
-    // Writing the updated notes to the db.json file
-    fs.writeFileSync('db/db.json', JSON.stringify(updatedNotes));
-    res.json(updatedNotes);
-  });
-};
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 // Dependencies
 const express = require('express');
 
@@ -23,4 +24,32 @@ require('./routes/htmlRoutes')(app);
 app.listen(PORT, () => {
   console.log(`Server available at localhost${PORT}`);
 });
+*/
 
+const express = require('express');
+
+// Point Server to the route files
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Create an express server
+const app = express();
+
+// Set PORT
+const PORT = process.env.PORT || 3001;
+
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// Parse incoming JSON data
+app.use(express.json());
+
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Listener
+app.listen(PORT, () => {
+    console.log(`API server is ready on port ${PORT}!`);
+});
